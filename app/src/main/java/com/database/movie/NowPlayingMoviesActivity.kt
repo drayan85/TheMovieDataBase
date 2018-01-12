8c64c0be642fd3e23db24e535ea770944558f686
Ilanthirayan Paramanathan <theebankala@gmail.com>
2018-01-12 15:14:38 +0800

    First commit

---
+++ app/src/main/java/com/database/movie/NowPlayingMoviesActivity.kt
@@ -0,0 +1,12 @@
+package com.database.movie
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class NowPlayingMoviesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_now_playing_movies)
+    }
+}
