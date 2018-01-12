3f38f86b9bc332ab3924f6eda3bc84bbea74e962
Ilanthirayan Paramanathan <theebankala@gmail.com>
2018-01-12 20:51:46 +0800

    TheMovieDBDebugApplication is created.
    TheMovieDBDebugApplication extended from android TheMovieDBApplication class to implement to configure debug mode setting (Strict Mode, LeakCanary & Stetho)
    
    Issue: #03
    
    -----------------
    Created by Ilanthirayan(Drayan): https://github.com/drayan85

---
+++ app/src/debug/java/com/database/movie/TheMovieDBDebugApplication.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2018 Ilanthirayan Paramanathan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.database.movie;
+
+import android.os.StrictMode;
+
+import com.facebook.stetho.Stetho;
+import com.squareup.leakcanary.LeakCanary;
+
+/**
+ * @author Ilanthirayan Paramanathan <theebankala@gmail.com>
+ * @version 1.0.0
+ * @since 12th of January 2018
+ */
+public class TheMovieDBDebugApplication extends TheMovieDBApplication {
+
+    @Override
+    public void onCreate() {
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                .detectDiskReads()
+                .detectDiskWrites()
+                .detectNetwork()
+                .penaltyLog()
+                .build());
+
+        StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                .detectLeakedSqlLiteObjects()
+                .detectLeakedClosableObjects()
+                .penaltyLog()
+                .penaltyDeath()
+                .build());
+
+        super.onCreate();
+
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
+
+        Stetho.initializeWithDefaults(this);
+    }
+}
