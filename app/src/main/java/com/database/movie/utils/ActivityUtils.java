3e2aa688a6e942ca9782896c80363d38c7d9c290
Ilanthirayan Paramanathan <theebankala@gmail.com>
2018-01-12 21:38:56 +0800

    ActivityUtils is created.
    ActivityUtils utility class has been implemented support Activity Generic methods(add fragment to the container)
    
    Issue: #03
    
    -----------------
    Created by Ilanthirayan(Drayan): https://github.com/drayan85

---
+++ app/src/main/java/com/database/movie/utils/ActivityUtils.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2018 Ilanthirayan Paramanathan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.database.movie.utils;
+
+import android.annotation.SuppressLint;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+
+import static android.support.v4.util.Preconditions.checkNotNull;
+
+/**
+ * @author Ilanthirayan Paramanathan <theebankala@gmail.com>
+ * @version 1.0.0
+ * @since 12th of January 2018
+ */
+public class ActivityUtils {
+
+    /**
+     * The {@code fragment} is added to the container view with id {@code frameId}. The operation is
+     * performed by the {@code fragmentManager}.
+     *
+     */
+    @SuppressLint("RestrictedApi")
+    public static void addFragmentToActivity (@NonNull FragmentManager fragmentManager,
+                                              @NonNull Fragment fragment, int frameId) {
+        checkNotNull(fragmentManager);
+        checkNotNull(fragment);
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(frameId, fragment, fragment.getClass().getName());
+        transaction.commitAllowingStateLoss();
+    }
+}
