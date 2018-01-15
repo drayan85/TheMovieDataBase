e17bb4c23c41cbb3ba1feb7393680eaba79bee69
Ilanthirayan Paramanathan <theebankala@gmail.com>
2018-01-15 14:48:21 +0800

    UseCase class is created
    UseCase domain layer abstract class to execute task in out side the main thread and pass the result back to main thread back
    
    Issue: #03
    
    -----------------
    Created by Ilanthirayan(Drayan): https://github.com/drayan85

---
+++ app/src/main/java/com/database/movie/domain_layer/usecase/UseCase.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2018 Ilanthirayan Paramanathan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.database.movie.domain_layer.usecase;
+
+import com.database.movie.domain_layer.executor.PostExecutionThread;
+import com.database.movie.domain_layer.executor.ThreadExecutor;
+
+import dagger.internal.Preconditions;
+import io.reactivex.Observable;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.observers.DisposableObserver;
+import io.reactivex.schedulers.Schedulers;
+
+/**
+ * Abstract class for a Use Case (Interactor in terms of Clean Architecture).
+ * This interface represents a execution unit for different use cases (this means any use case
+ * in the application should implement this contract).
+ *
+ * By convention each UseCase implementation will return the result using a {@link DisposableObserver}
+ * that will execute its job in a background thread and will post the result in the UI thread.
+ *
+ * @author Ilanthirayan Paramanathan <theebankala@gmail.com>
+ * @version 1.0.0
+ * @since 15th of January 2018
+ */
+public abstract class UseCase {
+
+
+    private final ThreadExecutor mThreadExecutor;
+    private final PostExecutionThread mPostExecutionThread;
+    private CompositeDisposable mDisposables;
+
+    public UseCase(ThreadExecutor threadExecutor, PostExecutionThread postExecutionThread) {
+        this.mThreadExecutor = threadExecutor;
+        this.mPostExecutionThread = postExecutionThread;
+        mDisposables = new CompositeDisposable();
+    }
+
+
+    public void execute(DisposableObserver disposableObserver, Observable observable){
+        final Observable<?> mObservable = observable
+                .subscribeOn(Schedulers.from(mThreadExecutor))
+                .observeOn(mPostExecutionThread.getScheduler());
+        addDisposable(mObservable.subscribeWith(disposableObserver));
+    }
+
+    /**
+     * Dispose from current {@link CompositeDisposable}.
+     */
+    public void dispose(){
+        if(!mDisposables.isDisposed()){
+            mDisposables.dispose();
+        }
+    }
+
+    /**
+     * Dispose from current {@link CompositeDisposable}.
+     */
+    private void addDisposable(Disposable disposable) {
+        Preconditions.checkNotNull(disposable);
+        Preconditions.checkNotNull(mDisposables);
+        mDisposables.add(disposable);
+    }
+}
