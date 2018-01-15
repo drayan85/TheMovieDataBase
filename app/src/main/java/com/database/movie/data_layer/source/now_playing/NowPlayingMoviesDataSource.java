9ca5ea02981adf247a8b89dc0cbdde49e42c8a4d
Ilanthirayan Paramanathan <theebankala@gmail.com>
2018-01-15 22:29:22 +0800

    NowPlayingMoviesDataSource interface is created
    NowPlayingMoviesDataSource data layer repository has been defined to be implemented in both repositories.
    
    Issue: #03
    
    -----------------
    Created by Ilanthirayan(Drayan): https://github.com/drayan85

---
+++ app/src/main/java/com/database/movie/data_layer/source/now_playing/NowPlayingMoviesDataSource.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2018 Ilanthirayan Paramanathan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.database.movie.data_layer.source.now_playing;
+
+import com.database.movie.data_layer.api.response.PaginatedMovies;
+
+import io.reactivex.Observable;
+
+/**
+ * @author Ilanthirayan Paramanathan <theebankala@gmail.com>
+ * @version 1.0.0
+ * @since 15th of January 2018
+ */
+public interface NowPlayingMoviesDataSource<T> {
+
+    /**
+     * Get an {@link Observable} which will emit a {@link T}
+     *
+     * @param current_page
+     * @param per_page
+     * @return {@link Observable} <{@link T}>
+     */
+    Observable<T> getNowPlayingMovies(int current_page, int per_page);
+
+    Observable saveNowPlayingMovies(PaginatedMovies paginatedMovies);
+}
