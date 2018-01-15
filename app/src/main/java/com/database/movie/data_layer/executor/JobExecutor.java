76f65f33c1fba7f928433b5f620d06357e615b06
Ilanthirayan Paramanathan <theebankala@gmail.com>
2018-01-15 13:00:26 +0800

    JobExecutor class is created
    JobExecutor implemented  from ThreadExecutor to define the Thread Pool Executor
    
    Issue: #03
    
    -----------------
    Created by Ilanthirayan(Drayan): https://github.com/drayan85

---
+++ app/src/main/java/com/database/movie/data_layer/executor/JobExecutor.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2018 Ilanthirayan Paramanathan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.database.movie.data_layer.executor;
+
+import android.support.annotation.NonNull;
+
+import com.database.movie.domain_layer.executor.ThreadExecutor;
+
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+/**
+ * Decorated {@link ThreadPoolExecutor}
+ *
+ * @author Ilanthirayan Paramanathan <theebankala@gmail.com>
+ * @version 1.0.0
+ * @since 15th of January 2018
+ */
+@Singleton
+public class JobExecutor implements ThreadExecutor {
+    /*
+    * Number of cores to decide the number of threads
+    */
+    public static final int NUMBER_OF_CORES = Runtime.getRuntime().availableProcessors();
+
+    private final ThreadPoolExecutor threadPoolExecutor;
+
+    @Inject
+    JobExecutor() {
+        this.threadPoolExecutor = new ThreadPoolExecutor(NUMBER_OF_CORES * 2, NUMBER_OF_CORES * 2,
+                60L, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>(), new JobThreadFactory());
+    }
+
+    @Override
+    public void execute(@NonNull Runnable runnable) {
+        this.threadPoolExecutor.execute(runnable);
+    }
+
+    private static class JobThreadFactory implements ThreadFactory {
+        private int counter = 0;
+
+        @Override
+        public Thread newThread(@NonNull Runnable runnable) {
+            return new Thread(runnable, "the_movie_db_" + counter++);
+        }
+    }
+}
