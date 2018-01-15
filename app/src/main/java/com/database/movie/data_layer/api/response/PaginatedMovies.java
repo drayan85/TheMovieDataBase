071639d1adfa01fef6a3aec6d9e698595c8616c7
Ilanthirayan Paramanathan <theebankala@gmail.com>
2018-01-15 21:29:55 +0800

    PaginatedMovies class is created
    PaginatedMovies class is created to read response body of paginated Movie list
    
    Issue: #03
    
    -----------------
    Created by Ilanthirayan(Drayan): https://github.com/drayan85

---
+++ app/src/main/java/com/database/movie/data_layer/api/response/PaginatedMovies.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2018 Ilanthirayan Paramanathan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.database.movie.data_layer.api.response;
+
+import com.database.movie.data_layer.model.Movie;
+import com.database.movie.data_layer.model.Movie_Dates;
+
+/**
+ * @author Ilanthirayan Paramanathan <theebankala@gmail.com>
+ * @version 1.0.0
+ * @since 15th of January 2018
+ */
+public class PaginatedMovies {
+
+    private Movie[] results;
+    private int page;
+    private int total_results;
+    private Movie_Dates dates;
+    private int total_pages;
+
+    public Movie[] getResults() {
+        return results;
+    }
+
+    public void setResults(Movie[] results) {
+        this.results = results;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getTotal_results() {
+        return total_results;
+    }
+
+    public void setTotal_results(int total_results) {
+        this.total_results = total_results;
+    }
+
+    public Movie_Dates getDates() {
+        return dates;
+    }
+
+    public void setDates(Movie_Dates dates) {
+        this.dates = dates;
+    }
+
+    public int getTotal_pages() {
+        return total_pages;
+    }
+
+    public void setTotal_pages(int total_pages) {
+        this.total_pages = total_pages;
+    }
+}
