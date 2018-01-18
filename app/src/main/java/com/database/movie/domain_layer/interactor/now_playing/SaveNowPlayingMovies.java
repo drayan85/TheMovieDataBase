d3417ea77f70e11b95eccf097793f70b160bc94f
Ilanthirayan Paramanathan <theebankala@gmail.com>
2018-01-18 12:57:10 +0800

    SaveNowPlayingMovies class is modified
    SaveNowPlayingMovies domain layer UseCase has been improved with new modified domain layer UseCase abstract class
    
    Issue: #03
    
    -----------------
    Created by Ilanthirayan(Drayan): https://github.com/drayan85

--- app/src/main/java/com/database/movie/domain_layer/interactor/now_playing/SaveNowPlayingMovies.java
+++ app/src/main/java/com/database/movie/domain_layer/interactor/now_playing/SaveNowPlayingMovies.java
@@ -13,24 +13,29 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.database.movie.domain_layer.usecase.now_playing;
+package com.database.movie.domain_layer.interactor.now_playing;
 
 import com.database.movie.data_layer.api.response.PaginatedMovies;
 import com.database.movie.domain_layer.executor.PostExecutionThread;
 import com.database.movie.domain_layer.executor.ThreadExecutor;
 import com.database.movie.domain_layer.repository.NowPlayingMoviesRepository;
-import com.database.movie.domain_layer.usecase.UseCase;
+import com.database.movie.domain_layer.interactor.UseCase;
 
 import javax.inject.Inject;
 
+import dagger.internal.Preconditions;
 import io.reactivex.Observable;
 
 /**
+ *
+ * This class is an implementation of {@link UseCase} that represents a use case for
+ * save NowPlaying Movies in SQLite Table {@link PaginatedMovies}.
+ *
  * @author Ilanthirayan Paramanathan <theebankala@gmail.com>
  * @version 1.0.0
  * @since 15th of January 2018
  */
-public class SaveNowPlayingMovies extends UseCase {
+public class SaveNowPlayingMovies extends UseCase<Boolean, PaginatedMovies> {
 
     private NowPlayingMoviesRepository mNowPlayingMoviesRepository;
 
@@ -42,7 +47,10 @@ public class SaveNowPlayingMovies extends UseCase {
         this.mNowPlayingMoviesRepository = mNowPlayingMoviesRepository;
     }
 
-    public Observable buildUseCaseObservable(PaginatedMovies paginatedMovies){
+
+    @Override
+    public Observable<Boolean> buildUseCaseObservable(PaginatedMovies paginatedMovies){
+        Preconditions.checkNotNull(paginatedMovies);
         return mNowPlayingMoviesRepository.saveNowPlayingMovies(paginatedMovies);
     }
 }
