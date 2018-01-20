22d5f3a687536da864b6349753f2ed46fd15b270
Ilanthirayan Paramanathan <theebankala@gmail.com>
2018-01-20 18:18:22 +0800

    code clean up
    Issue: #3
    
    -----------------
    Created by Ilanthirayan(Drayan): https://github.com/drayan85

--- app/src/androidTest/java/com/database/movie/presentation_layer/main/MainActivityTest.java
+++ app/src/androidTest/java/com/database/movie/presentation_layer/main/MainActivityTest.java
@@ -27,11 +27,8 @@ import android.view.View;
 
 import com.database.movie.R;
 
-import org.junit.After;
-import org.junit.Before;
 
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.contrib.DrawerMatchers.isOpen;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -39,7 +36,6 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.Assert.*;
 
 /**
  * @author Ilanthirayan Paramanathan <theebankala@gmail.com>
